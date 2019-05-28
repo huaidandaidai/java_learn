192d5cee83b3c0eed175d4b91aa9687a39ca1b40
lishanhui <1004928908@qq.com>
2019-05-28 14:52:17 +0800

    JVM作业

--- learn_data_structure/src/org/enjoy/homework/ConstantPool.java
+++ learn_data_structure/src/org/enjoy/homework/ConstantPool.java
@@ -2,18 +2,21 @@ package org.enjoy.homework;
 
 public class ConstantPool {
     public static void main(String[] args) {
-        String a ="a";
-        String b ="b";
-        String c="c";
-        String d= a+b+c;
-        d.intern();
-        String e ="abc";
-        String f =new String("abc");
-        System.out.println(d == e);
-        System.out.println(d == f);
-        System.out.println(e == f);
-        System.out.println(d.intern() == d);
-        System.out.println(d.intern() == e);
-        System.out.println(d.intern() == f);
+        String b = "享学";
+        String a = b + "课堂";
+        System.out.println(a.intern() == a);
+//        String a ="a";
+//        String b ="b";
+//        String c="c";
+//        String d= a+b+c;
+//        d.intern();
+//        String e ="abc";
+//        String f =new String("abc");
+//        System.out.println(d == e);
+//        System.out.println(d == f);
+//        System.out.println(e == f);
+//        System.out.println(d.intern() == d);
+//        System.out.println(d.intern() == e);
+//        System.out.println(d.intern() == f);
     }
 }
