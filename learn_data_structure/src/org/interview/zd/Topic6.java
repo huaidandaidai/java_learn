cbb6c70a7491f85b3438ec112f8ce8b4ccc872fa
lishanhui <1004928908@qq.com>
2019-07-03 14:14:27 +0800

    面试题

--- learn_data_structure/src/org/interview/zd/Topic6.java
+++ learn_data_structure/src/org/interview/zd/Topic6.java
@@ -1,40 +1,39 @@
 package org.interview.zd;
 
-import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 一个字符串有n个字符，
+ * 从第一个字符开始数，第m个字符从串中移除并输出，再接着从下一个字符继续数，
+ * 第m个从串中移除并输出，数到末尾从头接着数，直到串中小于m个字符为止
+ */
 public class Topic6 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int arr[] = new int[N];
-        int i = 0;
-        int callNum = 0;
-        int outNum = 0;
-        while (true) {
-            //如果未置成1
-            if (arr[i] == 0) {
-                //如果未出局人数为一，结束循环
-                if (arr.length - outNum == 1) {
-                    break;
-                } else {
-                    //报数
-                    callNum++;
-                    //当报数值为3的时候
-                    if (callNum % 3 == 0) {
-                        //置值为1
-                        arr[i] = 1;
-                        //出局人数加1
-                        outNum++;
-                        //重置报数
-                        callNum = 0;
-                    }
-                }
-            }
-            //下标跳跃到下一个
-            i++;
-            //当下标与N
-            i = i % N;
+        yuesefu(10, 3);
+    }
+    public static void yuesefu(int totalNum, int countNum) {
+        // 初始化人数
+        List<Integer> start = new ArrayList<Integer>();
+        for (int i = 1; i <= totalNum; i++) {
+            start.add(i);
         }
-        System.out.println(i + 1);
+        // 从第K个开始计数
+        int k = 0;
+        while (start.size() > 0) {
+            k = k + countNum;
+            // 第m人的索引位置
+            k = k % start.size() - 1;
+            // 判断是否到队尾
+            if (k < 0) {
+                System.out.println(start.get(start.size() - 1));
+                start.remove(start.size() - 1);
+                k = 0;
+            } else {
+                System.out.println(start.get(k));
+                start.remove(k);
+            }
+        }
     }
 }
